e12e90df5325c5d48f979e0f436e6913848cf059
Chongyi <xpz3847878@163.com>
2021-12-26 21:55:51 +0800

    初始化项目

---
+++ inspirer-apps/inspirer-blog/migrations/20211226130830_initialize_tables.down.sql
@@ -0,0 +1,3 @@
+drop table if exists `inspirer_blog_users`;
+drop table if exists `inspirer_blog_contents`;
+drop table if exists `inspirer_blog_content_body`;
---
+++ inspirer-core/src/lib.rs
@@ -0,0 +1,11 @@
+mod framework;
+mod application;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
